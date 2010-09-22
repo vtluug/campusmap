1051ad49bd1f020a9f3df591fe7b7b7a8442e37e
mutantmonkey <mutantmonkey@gmail.com>
2010-09-21 22:33:54 -0400

    Reorganized Javascript files and added some more comments

---
+++ js/geolocation.js
@@ -0,0 +1,48 @@
+/**
+ * Add a "Where Am I?" button to the map if geolocation is supported by the browser
+ */
+function addLocateButton()
+{
+	if(!navigator.geolocation)
+	{
+		// button should not be added if feature is not supported by browser
+	}
+
+	atag = document.createElement('a');
+	atag.className = 'fake_button';
+	atag.href = 'javascript:void(null)';
+	atag.innerHTML = "Where Am I?";
+
+	atag.addEventListener('mousedown', showMyLocation, true);
+
+	$('#map_toolbar').prepend(atag);
+}
+
+/**
+ * Zoom to the location returned by the browser
+ */
+function showMyLocation(e)
+{
+	if(navigator.geolocation)
+	{
+		navigator.geolocation.getCurrentPosition(markAndZoomGeo);
+	}
+}
+
+/**
+ * Add a marker and zoom to the specified location
+ */
+function markAndZoomGeo(position)
+{
+	loc			= new OpenLayers.LonLat(position.coords.longitude, position.coords.latitude).
+					transform(EPSG_4326, map.getProjectionObject());
+
+	// add green marker to indicate location
+	size		= new OpenLayers.Size(21, 25);
+	icon		= new OpenLayers.Icon('js/openlayers/img/marker-green.png', size);
+	markers.addMarker(new OpenLayers.Marker(loc, icon));
+
+	// zoom in a bit
+	map.setCenter(loc, CLOSER_ZOOM);
+}
+
---
+++ js/map.js
@@ -0,0 +1,85 @@
+// initial location and zoom
+lat = 37.231797472275744;
+lon = -80.42846213700699;
+INITIAL_ZOOM = 14;
+CLOSER_ZOOM = 17;
+
+EPSG_4326 = new OpenLayers.Projection('EPSG:4326');
+
+AutoSizeAnchored = OpenLayers.Class(OpenLayers.Popup.Anchored, { 'autoSize': true });
+AutoSizeAnchoredBubble = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, { 'autoSize': true });
+
+var map;
+
+/**
+ * Initialize the map
+ */
+function initMap()
+{
+	map		 = new OpenLayers.Map('map', {
+		controls			: [
+			new OpenLayers.Control.Navigation(),
+			new OpenLayers.Control.PanZoomBar(),
+			//new OpenLayers.Control.LayerSwitcher({'ascending':false}),
+			new OpenLayers.Control.Attribution()
+		],
+		units				: 'm',
+		//projection			: new OpenLayers.Projection("EPSG:900913"),
+		//displayProjection	: new OpenLayers.Projection("EPSG:4326"),
+	});
+
+	// OpenStreetMap base layer
+	mapnik = new OpenLayers.Layer.OSM.Mapnik('OpenStreetMap');
+	map.addLayer(mapnik);
+
+	// Markers layer
+	markers = new OpenLayers.Layer.Markers('Markers');
+	map.addLayer(markers);
+
+	// center map on Blacksburg
+	loc			= new OpenLayers.LonLat(lon, lat).transform(
+		EPSG_4326, map.getProjectionObject());
+	map.setCenter(loc, INITIAL_ZOOM);
+}
+
+
+/**
+ * Add a marker to the specified layer
+ */
+function addMarker(layer, ll, popupClass, popupContent, data)
+{
+	feature = new OpenLayers.Feature(layer, ll);
+	feature.closeBox = data['closeBox'];
+	feature.popupClass = popupClass;
+	feature.data.popupContentHTML = popupContent;
+	feature.data.overflow = 'auto';
+
+	if(data['icon'])
+	{
+		feature.data.icon = data['icon'];
+	}
+
+	marker = feature.createMarker();
+
+	markerClick = function(e)
+	{
+		if(this.popup == null)
+		{
+			this.popup = this.createPopup(this.closeBox);
+			map.addPopup(this.popup);
+			this.popup.show();
+		}
+		else {
+			this.popup.toggle();
+		}
+
+		currentPopup = this.popup;
+
+		// stop the event here before it does any serious damage!
+		OpenLayers.Event.stop(e);
+	};
+	marker.events.register('mousedown', feature, markerClick);
+
+	layer.addMarker(marker);
+}
+
